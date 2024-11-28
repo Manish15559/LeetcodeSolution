367a782c3c31e2092b4c951168f137bc2e4f741e
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2024-11-28 22:50:36 +0530

    Time: 8 ms (95.67%), Space: 210.5 MB (42.92%) - LeetHub

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -21,37 +21,39 @@ TreeNode *lcs(TreeNode *node, int startValue, int destValue){
     else if(r==NULL) return l;
     return node;
 }
-bool helper1(TreeNode *node,int start,string &ans){
-    if(node==NULL) return false;
-    if(node->val==start) return true;
+bool helper1(TreeNode *node,int startValue,string &ans){
+    if(node==NULL){
+        return false;
+    }
+    if(node->val==startValue) return true;
     ans.push_back('U');
-    if(helper1(node->left,start,ans)==true) return true;
-   
-    if(helper1(node->right,start,ans)==true) return true;
+    if(helper1(node->left,startValue,ans)==true) return true;
+    if(helper1(node->right,startValue,ans)==true) return true;
     ans.pop_back();
     return false;
+
 }
-bool helper2(TreeNode *node,int dest,string &ans){
-    if(node==NULL) return false;
-    if(node->val==dest) return true;
+bool helper2(TreeNode *node,int destValue,string &ans){
+    if(node==NULL){
+        return false;
+    }
+    if(node->val==destValue) return true;
     ans.push_back('L');
-    if(helper2(node->left,dest,ans)==true) return true;
+    if(helper2(node->left,destValue,ans)==true) return true;
     ans.pop_back();
-     ans.push_back('R');
-    if(helper2(node->right,dest,ans)==true) return true;
+    ans.push_back('R');
+    if(helper2(node->right,destValue,ans)==true) return true;
     ans.pop_back();
     return false;
+
 }
     string getDirections(TreeNode* root, int startValue, int destValue) {
 
-
-        TreeNode * ancestor=lcs(root,startValue,destValue);
-
+        TreeNode *ansector=lcs(root,startValue,destValue);
         string ans="";
-        helper1(root,startValue,ans);
-        helper2(root,destValue,ans);
-        return ans;
-
+        helper1(ansector,startValue,ans);
+        helper2(ansector,destValue,ans);
+return ans;
         
     }
 };
