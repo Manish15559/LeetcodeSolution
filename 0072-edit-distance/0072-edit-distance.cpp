aa528491709961aaf851eca62c48fc9125da9e44
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2024-12-16 17:07:59 +0530

    Time: 10 ms (33.05%), Space: 13.2 MB (27.72%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,34 +3,44 @@ public:
 
     int minDistance(string word1, string word2) {
 
-        int n1=word1.size();
-        int n2=word2.size();
+    int length1 = word1.size(); // Length of word1
+    int length2 = word2.size(); // Length of word2
 
-        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+    // Create a DP table to store the minimum edit distances
+    vector<vector<int>> dp(length1 + 1, vector<int>(length2 + 1, 0));
 
-        for(int idx1=0;idx1<=n1;idx1++){
-            dp[idx1][0]=idx1;
-        }
-          for(int idx2=0;idx2<=n2;idx2++){
-            dp[0][idx2]=idx2;
-        }
-
-        for(int idx1=1;idx1<=n1;idx1++){
-            for(int idx2=1;idx2<=n2;idx2++){
-
-                if(word1[idx1-1]==word2[idx2-1]){
-       dp[idx1][idx2]=dp[idx1-1][idx2-1];
-    }
-    else{
-dp[idx1][idx2]= min({dp[idx1][idx2-1],dp[idx1-1][idx2],dp[idx1-1][idx2-1]})+1;
+    // Initialize the base cases:
+    // If word2 is empty, we need to delete all characters of word1
+    for (int i = 0; i <= length1; i++) {
+        dp[i][0] = i;
     }
 
-    
+    // If word1 is empty, we need to insert all characters of word2
+    for (int j = 0; j <= length2; j++) {
+        dp[0][j] = j;
+    }
 
+    // Fill the DP table using the recurrence relation
+    for (int i = 1; i <= length1; i++) {
+        for (int j = 1; j <= length2; j++) {
+            // If the characters are the same, no operation is needed
+            if (word1[i - 1] == word2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } 
+            // If the characters are different, choose the minimum of the three operations:
+            // Insert, Delete, Replace
+            else {
+                dp[i][j] = 1 + min({
+                    dp[i][j - 1],    // Insert a character
+                    dp[i - 1][j],    // Delete a character
+                    dp[i - 1][j - 1] // Replace a character
+                });
             }
         }
-        return dp[n1][n2];
+    }
 
+    // The result is stored in the last cell of the DP table
+    return dp[length1][length2];
 
         
     }
