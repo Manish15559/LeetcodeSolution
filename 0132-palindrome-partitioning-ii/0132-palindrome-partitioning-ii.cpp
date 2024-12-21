e86ecd3a181ce215e479580807485b93e2097338
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2024-12-21 17:13:41 +0530

    Time: 1529 ms (35.17%), Space: 9.2 MB (69.1%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-bool isPalindrome(int l,int r,string &str){
-
-   
-    while(r>l){
-        if(str[l++]!=str[r--]) return false;
+bool isPalindrome(int l, int r, string &s) {
+    while (r > l) {
+        if (s[l++] != s[r--]) return false;
     }
     return true;
-
 }
 
 
     int minCut(string s) {
         int n=s.size();
+       
+ vector<int> dp(n + 1, 0);
 
-        vector<int>dp(n+1,0);
-      for(int idx=1;idx<=n;idx++){
-        int mini=1e9;
-        for(int left=idx;left>=1;left--){
-            if(isPalindrome(left-1,idx-1,s)==true) mini=min(mini,1+dp[left-1]);
+    for (int i = 1; i <= n; i++) {
+        int mini = INT_MAX; // Large value to track minimum cuts
+        for (int j = i; j >= 1; j--) {
+            if (isPalindrome(j - 1, i - 1, s)) {
+                if (j == 1) {
+                    mini = 0; // No cuts needed for the whole substring
+                } else {
+                    mini = min(mini, dp[j - 1] + 1); // Add 1 cut
+                }
+            }
         }
-        dp[idx]=mini;
-      }
-        
+        dp[i] = mini;
+    }
+    return dp[n];
+      
 
-        return dp[n]-1;
     }
 };
