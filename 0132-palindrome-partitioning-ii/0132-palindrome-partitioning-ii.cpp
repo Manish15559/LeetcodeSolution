0ef81df7ede89274173fc73edacc340a67534ea0
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2024-12-27 09:51:19 +0530

    Time: 34 ms (94.82%), Space: 12.1 MB (45.37%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
-bool isPalindrome(int l, int r, string &s) {
-    while (r > l) {
-        if (s[l++] != s[r--]) return false;
+void checkPalindrome(int l,int r,string &s,vector<vector<bool>>&isPalindrome,int n){
+    while(l>=0&&r<n){
+        if(s[l]==s[r]){
+            isPalindrome[l][r]=true;
+            l--;
+            r++;
+        }
+        else{
+            break;
+        }
     }
-    return true;
+    return;
 }
 
 
     int minCut(string s) {
         int n=s.size();
        
- vector<int> dp(n + 1, 0);
+   vector<int> dp(n + 1, 0); // dp[i] stores minimum cuts needed for s[0...i-1]
+
+    vector<vector<bool>>isPalindrome((n),vector<bool>((n),false));
+
+    for(int idx=0;idx<n;idx++){
+        checkPalindrome(idx,idx,s,isPalindrome,n);
+        if(idx<(n-1))
+        checkPalindrome(idx,idx+1,s,isPalindrome,n);
+    }
 
     for (int i = 1; i <= n; i++) {
-        int mini = INT_MAX; // Large value to track minimum cuts
+        int mini = INT_MAX; // Initialize with a large value
         for (int j = i; j >= 1; j--) {
-            if (isPalindrome(j - 1, i - 1, s)) {
+            if (isPalindrome[j-1][i-1]) {
                 if (j == 1) {
-                    mini = 0; // No cuts needed for the whole substring
+                    mini = 0; // No cuts needed if s[0...i-1] itself is a palindrome
                 } else {
-                    mini = min(mini, dp[j - 1] + 1); // Add 1 cut
+                    mini = min(mini, dp[j - 1] + 1); // Update dp[i] with minimum cuts
                 }
             }
         }
-        dp[i] = mini;
+        dp[i] = mini; // Store the minimum cuts for s[0...i-1]
     }
-    return dp[n];
-      
+    return dp[n]; // Return the result for the whole string
 
     }
 };
