bacba8c4ca06b616df12c40a8e84148962b030a5
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-07-03 20:47:36 +0530

    Time: 0 ms (100%), Space: 8.3 MB (47.6%) - LeetHub

--- 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.cpp
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.cpp
@@ -2,29 +2,21 @@ class Solution {
 public:
     bool isRobotBounded(string instructions) {
 
-    set<pair<int,int>>visited;
+   
     vector<int>dx={0,1,0,-1};
     vector<int>dy={1,0,-1,0};
     int idx=0;
     int x=0;
     int y=0;
     int n=instructions.size();
-     visited.insert({x,y});
-     int cnt=0;
-     bool f=true;
+    
 
-    for(int i=0;i<100*n;i++){
+    for(int i=0;i<n;i++){
         if(instructions[i%n]=='G'){  
-            f=false;
+           
             x=x+dx[idx];
             y=y+dy[idx];
-            if(visited.find({x,y})!=visited.end()) cnt++;
-            else{
-                cnt=0;
-                visited.insert({x,y});
-            }
-            if(cnt==visited.size()) return true;
-            
+           
         }
         else if(instructions[i%n]=='R'){
             idx=(idx+1)%4;
@@ -34,7 +26,7 @@ public:
         
        
     }
-    return f;
+    return x==0 && y==0 || idx!=0;
    
         
     }
