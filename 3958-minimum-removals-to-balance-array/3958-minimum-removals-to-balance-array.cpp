f3b5cf51eca032afd6583630156574d4b7f27c18
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-08-04 19:07:20 +0530

    Time: 27 ms (66.67%), Space: 105.1 MB (0%) - LeetHub

--- 3958-minimum-removals-to-balance-array/3958-minimum-removals-to-balance-array.cpp
+++ 3958-minimum-removals-to-balance-array/3958-minimum-removals-to-balance-array.cpp
@@ -4,13 +4,11 @@ public:
         int n=nums.size();
         int ans=(n-1);
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-            long long val=(long long)k*nums[i];
-            int idx=upper_bound(nums.begin(),nums.end(),val)-nums.begin();
-            idx--;
-            int removeCnt = n - (idx-i+1);
-            ans=min(ans,removeCnt);
-        }
+       int left=0;
+       for(int right=0;right<n;right++){
+        if(nums[right]> (long)k*nums[left]) left++;
+        ans=min(ans,(n-(right-left+1)));
+       }
         return ans;
         
     }
