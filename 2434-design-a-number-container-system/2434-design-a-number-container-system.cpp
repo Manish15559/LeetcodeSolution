356d5267badca8ba0bd48fa9072163933d2d1c3c
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-09-21 19:20:39 +0530

    Time: 169 ms (51.39%), Space: 267.6 MB (48.84%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -1,26 +1,27 @@
 class NumberContainers {
 public:
-    map<int,int>idxer; 
-    map<int,priority_queue<int,vector<int>,greater<int>>>num;
+    unordered_map<int,int>indexToNumber; //it will store at which index what number is store
+    unordered_map<int,set<int>>numberToindices; //it will store for a given number what are position where the index is storing number in sorted order
     NumberContainers() {
         
     }
     
     void change(int index, int number) {
-        num[number].push(index);
-        idxer[index]=number;
+       if(indexToNumber.find(index)!=indexToNumber.end()){
+        int previousNumber = indexToNumber[index];
+        numberToindices[previousNumber].erase(index);
+        if(numberToindices[previousNumber].empty()) numberToindices.erase(previousNumber);
+       }
+       indexToNumber[index]=number;
+       numberToindices[number].insert(index);
         
     }
     
     int find(int number) {
 
-        while(!num[number].empty()){
-            int idx=num[number].top();
-            if(idxer[idx]!=number) num[number].pop();
-            else return idx;
-        }
+       if(numberToindices.find(number)!=numberToindices.end()) return *numberToindices[number].begin();
 
-        return -1;
+       return -1;
         
     }
 };
