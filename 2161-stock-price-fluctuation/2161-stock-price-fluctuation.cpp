24ed69443fb0a8fe297d89ab068b521d904db212
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-09-28 12:23:56 +0530

    Time: 131 ms (35.73%), Space: 173.9 MB (52.62%) - LeetHub

---
+++ 2161-stock-price-fluctuation/2161-stock-price-fluctuation.cpp
@@ -0,0 +1,54 @@
+class StockPrice {
+public:
+
+multiset<int>st; //track all the prices from low value to high value;
+unordered_map<int,int>mp; //track the price at particular timestamp
+int latest_timestamp=-1;
+    StockPrice() {
+        
+    }
+    
+    void update(int timestamp, int price) {  //2*o(log(numberofuniquetimestamp))
+        if(mp.find(timestamp)==mp.end()){
+            mp[timestamp]=price;
+            st.insert(price);
+            latest_timestamp=max(timestamp,latest_timestamp);
+
+        }
+        else{
+            st.erase(st.find(mp[timestamp]));
+            st.insert(price);
+            mp[timestamp]=price;
+
+        }
+        
+        
+    }
+    
+    int current() {
+
+    return mp[latest_timestamp];
+        
+    }
+    
+    int maximum() {
+
+        return *st.rbegin();
+        
+    }
+    
+    int minimum() {
+
+        return *st.begin();
+        
+    }
+};
+
+/**
+ * Your StockPrice object will be instantiated and called as such:
+ * StockPrice* obj = new StockPrice();
+ * obj->update(timestamp,price);
+ * int param_2 = obj->current();
+ * int param_3 = obj->maximum();
+ * int param_4 = obj->minimum();
+ */
