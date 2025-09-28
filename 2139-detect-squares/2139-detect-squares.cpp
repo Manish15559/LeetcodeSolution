46d705ef313c6b47fed3c45fce0f830fc05c3f6a
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-09-28 22:35:58 +0530

    Time: 93 ms (24.37%), Space: 153.9 MB (17.67%) - LeetHub

--- 2139-detect-squares/2139-detect-squares.cpp
+++ 2139-detect-squares/2139-detect-squares.cpp
@@ -3,42 +3,33 @@ using namespace std;
 
 class DetectSquares {
 public:
-   
-    map<pair<int,int>,int>cnt;
+   static const int N=1001;
+   int cnt[N][N]={0};
+   vector<pair<int,int>> points;
    
     DetectSquares() {
     
     }
     
     void add(vector<int> point) {
-        int x_coordinate=point[0];
-        int y_coordinate=point[1];
-        cnt[{x_coordinate,y_coordinate}]++;
+       points.push_back({point[0],point[1]});
+       cnt[point[0]][point[1]]++;
         
     }
-    int cal(int x,int y,int dx,int dy){
-        int c=0;
-        int old_x=x;
-        int old_y=y;
-        while(x>=0&&x<=1000&&y>=0&&y<=1000){
-            x+=dx;
-            y+=dy;
-            if(cnt.find({x,old_y})!=cnt.end()&&cnt.find({old_x,y})!=cnt.end()&&cnt.find({x,y})!=cnt.end()) c+=(cnt[{old_x,y}]*cnt[{x,old_y}]*cnt[{x,y}]);
-        }
-        return c;
-    }
+   
     
     int count(vector<int> point) {
-        int c=0;
-
-        //first quadrant
+        int ans=0;
         int x=point[0];
         int y=point[1];
-        int dx[]={-1,1,1,-1};
-        int dy[]={1,1,-1,-1};
-        for(int i=0;i<4;i++) c+=cal(x,y,dx[i],dy[i]);
 
-        return c;
+        for(auto&[x1,y1] : points){
+            if(abs(x-x1)>0&&abs(x-x1)==abs(y-y1)){
+                ans+= cnt[x][y1]*cnt[x1][y];
+            }
+        }
+
+       return ans;
 
 
         
