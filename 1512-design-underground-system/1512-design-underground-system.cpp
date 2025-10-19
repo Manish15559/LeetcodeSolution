ea7dbee30d93d3b92748735a826a4c4f1fc0a6aa
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-10-19 14:17:43 +0530

    Time: 42 ms (19.6%), Space: 68.2 MB (51.24%) - LeetHub

--- 1512-design-underground-system/1512-design-underground-system.cpp
+++ 1512-design-underground-system/1512-design-underground-system.cpp
@@ -1,27 +1,45 @@
 class UndergroundSystem {
 public:
-    unordered_map<int, pair<string, int>> checkIns; // id -> (station, time)
-    unordered_map<string, pair<long long, int>> routes; // "A B" -> (totalTime, count)
-
-    void checkIn(int id, string stationName, int t) {
-        checkIns[id] = {stationName, t};
+    unordered_map<int,pair<string,int>>checkIns;   //id -> {startStation, checkIn time}
+    unordered_map<string,pair<long long,int>>routes; // startStation endStation -> {totTime,count}
+    UndergroundSystem() {
+        
     }
-
+    
+    void checkIn(int id, string stationName, int t) {
+        checkIns[id] = {stationName,t};
+        
+    }
+    
     void checkOut(int id, string stationName, int t) {
         auto [startStation, startTime] = checkIns[id];
         checkIns.erase(id);
 
-        string route = startStation + " " + stationName;
-        int duration = t - startTime;
+        string route= startStation + " " + stationName;
+        long long duration = t - startTime;
 
-        auto& [total, cnt] = routes[route];
-        total += duration;
-        cnt += 1;
+        auto &[totTime, count] = routes[route];
+        totTime+=duration;
+        count+=1;
+        
+        
     }
-
+    
     double getAverageTime(string startStation, string endStation) {
-        string route = startStation + " " + endStation;
-        auto [total, cnt] = routes[route];
-        return (double)total / cnt;
+
+        string route= startStation + " " + endStation;
+
+        auto [totTime, count] = routes[route];
+
+        return ((double)totTime)/count;
+        
     }
 };
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem* obj = new UndergroundSystem();
+ * obj->checkIn(id,stationName,t);
+ * obj->checkOut(id,stationName,t);
+ * double param_3 = obj->getAverageTime(startStation,endStation);
+ */
