ee5c1099510176a64b64a0ca039c2e05e1c75b02
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-10-22 22:06:00 +0530

    Time: 118 ms (61.57%), Space: 97.7 MB (35.99%) - LeetHub

--- 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
+++ 3620-maximum-number-of-distinct-elements-after-operations/3620-maximum-number-of-distinct-elements-after-operations.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int maxDistinctElements(vector<int>& nums, int k) {
-        map<int,int>mp;
-        for(auto it:nums) mp[it]++;
-
-        int ans=0;
-        int used=-1e9;
-        for(auto it:mp){
-            int num=it.first;
-            int cnt=it.second;
-            int rangeStart=max(used,(it.first-k));
-            int rangeEnd=it.first+k;
-            if(cnt>(rangeEnd-rangeStart+1)){
-                ans+=(rangeEnd-rangeStart+1);
-                used=rangeEnd+1;
-            }
-            else{
-                ans+=cnt;
-                used=rangeStart+cnt;
-            }
-
+       sort(nums.begin(),nums.end());
+       int ans=0;
+       int prev=-1e9;
+       for(auto it:nums){
+        int l=max((prev+1),(it-k));
+        if(l<=(it+k)){
+            prev=l;
+            ans++;
         }
-        return ans;
+       }
+       return ans;
         
     }
 };
