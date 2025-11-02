bc15d61f03293f2ec7a1d165b1169e1ad2a332ac
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-11-02 18:31:10 +0530

    Time: 115 ms (55.17%), Space: 110.9 MB (60.03%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
-
-void dfs(int currNode,int previousNode,int &ans,vector<int>adj[],vector<int>reverseAdj[]){
+enum Orientation{ Original, Reverse};
+void dfs(int currNode,int previousNode,int &ans, vector<pair<int,int>>adj[]){
     // cout<<currNode<<endl;
     for(auto adjNode:adj[currNode]){
         // cout<<adjNode<<endl;
-        if(adjNode==previousNode) continue;
+        if(adjNode.first==previousNode) continue;
         //  cout<<previousNode<<" "<<currNode<<" "<<adjNode<<endl;
-        ans++;
-        dfs(adjNode,currNode,ans,adj,reverseAdj);
+        if(adjNode.second==1) ans++;
+        dfs(adjNode.first,currNode,ans,adj);
 
     }
-    for(auto adjNode:reverseAdj[currNode]){
-        if(adjNode==previousNode) continue;
-        dfs(adjNode,currNode,ans,adj,reverseAdj);
-
-    }
+    
 }
 
     int minReorder(int n, vector<vector<int>>& connections) {
 
-        vector<int>adj[n];
-        vector<int>reverseAdj[n];
+        vector<pair<int,int>>adj[n];
+        // vector<int>reverseAdj[n];
         for(auto connection:connections){
-            adj[connection[0]].push_back(connection[1]);
-            reverseAdj[connection[1]].push_back(connection[0]);
+            adj[connection[0]].push_back({connection[1],1});
+            adj[connection[1]].push_back({connection[0],0});
         }
         int oriented=0;
 
-        dfs(0,-1,oriented,adj,reverseAdj);
+        dfs(0,-1,oriented,adj);
         return oriented;
 
 
