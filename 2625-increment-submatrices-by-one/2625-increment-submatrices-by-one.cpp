190ca02e9a5e33884317b5ee95b0419f02612ccd
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-11-14 22:13:10 +0530

    Time: 31 ms (62.07%), Space: 91.3 MB (53.92%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -3,18 +3,22 @@ public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
         vector<vector<int>>arr(n,vector<int>(n,0));
         for(auto it: queries){  //o(q*n)
-            int row1=it[0];
+            int row1=it[0];  
             int col1=it[1];
             int row2=it[2];
             int col2=it[3];
-            for(int row=row1;row<=row2;row++){
-                arr[row][col1]++;
-                if((col2+1)<n) arr[row][col2+1]--;
-            }
+           arr[row1][col1]++;
+           if((col2+1)<n) arr[row1][col2+1]--;
+           if((row2+1)<n) arr[row2+1][col1]--;
+           if((col2+1)<n&&(row2+1)<n) arr[row2+1][col2+1]++;
         }
         for(int row=0;row<n;row++){  //o(n^2)
-            for(int col=1;col<n;col++){
-                arr[row][col]+=arr[row][col-1];
+            for(int col=0;col<n;col++){
+               int above = row ? arr[row-1][col] : 0;
+               int left= col ? arr[row][col-1] : 0;
+               int diagonal = (row&&col) ? arr[row-1][col-1] : 0;
+               arr[row][col]+=(left+above-diagonal);
+                
             }
         }
 
