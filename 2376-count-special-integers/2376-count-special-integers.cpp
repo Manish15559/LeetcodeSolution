cf997baee0a6b88dcb1ee553d2f4cdf1099a90d1
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-11-23 17:08:37 +0530

    Time: 453 ms (9.26%), Space: 8.3 MB (46.67%) - LeetHub

---
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+int dp[10][(1<<10)][2][2];
+   int helper(int idx,int mask,bool tight,bool zero,vector<int>&arr){
+
+    if(idx==arr.size()) {
+        cout<<mask<<endl;
+        return (zero==false);
+    }
+    
+    if(dp[idx][mask][tight][zero]!=-1) return dp[idx][mask][tight][zero];
+    int sum=0;
+    for(int num=0;num<=9;num++){
+        // cout<<num<<" "<<idx<<endl;
+        if((mask>>num)&1) continue;
+        if(tight==true&&num>arr[idx]) {
+            // cout<<idx<<" "<<num<<endl;
+            continue;
+        }
+        int ntight=(tight&&(num==arr[idx]));
+        int nzero=(zero&&(num==0));
+        int nmask=nzero ? mask: (mask|(1<<num));
+        // cout<<nzero<<endl;
+        sum+=helper(idx+1,nmask,ntight,nzero,arr);
+
+
+    }
+    return dp[idx][mask][tight][zero]=sum;
+   }
+
+    int countSpecialNumbers(int n) {
+        // int k=n;
+        vector<int>arr;
+        while(n>0){
+            arr.push_back(n%10);
+            n/=10;
+        }
+        reverse(arr.begin(),arr.end());
+        memset(dp,-1,sizeof(dp));
+        return (helper(0,0,true,true,arr));
+        
+    }
+};
