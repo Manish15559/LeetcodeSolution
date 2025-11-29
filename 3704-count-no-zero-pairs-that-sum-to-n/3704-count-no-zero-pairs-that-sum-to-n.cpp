4bd44cc4ed32e2f214c28c07f0b1aee7a6607e29
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-11-29 21:33:52 +0530

    Time: 7 ms (54.29%), Space: 9.4 MB (56.63%) - LeetHub

---
+++ 3704-count-no-zero-pairs-that-sum-to-n/3704-count-no-zero-pairs-that-sum-to-n.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+long long dp[16][2][2][2];
+    long long helper(int idx,int carry,int zero1,int zero2,vector<int>&arr){
+        if(idx==arr.size()) return carry==0;
+        
+        if(dp[idx][carry][zero1][zero2]!=-1) return dp[idx][carry][zero1][zero2];
+       
+        long long tot=0;
+        for(int i=0;i<=9;i++){
+            for(int j=0;j<=9;j++){
+                int curr=(i+j+carry);
+                // if(val==0)
+                if((curr%10)!=arr[idx]) continue;
+                if(idx==0&&(j==0)) continue;
+                if(idx==0&&i==0) continue;
+                if(zero1&&i>0) continue;
+                if(zero2&&j>0) continue;
+                int nzero1=zero1||(i==0);
+                int nzero2=zero2||(j==0);
+                tot+=helper(idx+1,(curr>=10),nzero1,nzero2,arr);
+            }
+        }
+        return dp[idx][carry][zero1][zero2]=tot;
+        
+    }
+    long long countNoZeroPairs(long long n) {
+        vector<int>arr;
+        while(n>0){
+            arr.push_back(n%10);
+            n/=10;
+        }
+        memset(dp,-1,sizeof(dp));
+        return helper(0,0,0,0,arr);
+
+        
+    }
+};
