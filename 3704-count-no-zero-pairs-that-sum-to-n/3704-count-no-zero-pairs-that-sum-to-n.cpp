2c93b2cd2448924c26218062f67d77d3a5d21495
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-11-29 21:38:30 +0530

    Time: 4 ms (68.99%), Space: 9.4 MB (56.63%) - LeetHub

--- 3704-count-no-zero-pairs-that-sum-to-n/3704-count-no-zero-pairs-that-sum-to-n.cpp
+++ 3704-count-no-zero-pairs-that-sum-to-n/3704-count-no-zero-pairs-that-sum-to-n.cpp
@@ -5,20 +5,22 @@ long long dp[16][2][2][2];
         if(idx==arr.size()) return carry==0;
         
         if(dp[idx][carry][zero1][zero2]!=-1) return dp[idx][carry][zero1][zero2];
+        int val=arr[idx]-carry;
+        val=(val+10)%10;
        
         long long tot=0;
         for(int i=0;i<=9;i++){
             for(int j=0;j<=9;j++){
-                int curr=(i+j+carry);
+                int curr=(i+j);
                 // if(val==0)
-                if((curr%10)!=arr[idx]) continue;
+                if((curr%10)!=val) continue;
                 if(idx==0&&(j==0)) continue;
                 if(idx==0&&i==0) continue;
                 if(zero1&&i>0) continue;
                 if(zero2&&j>0) continue;
                 int nzero1=zero1||(i==0);
                 int nzero2=zero2||(j==0);
-                tot+=helper(idx+1,(curr>=10),nzero1,nzero2,arr);
+                tot+=helper(idx+1,((curr+carry)>=10),nzero1,nzero2,arr);
             }
         }
         return dp[idx][carry][zero1][zero2]=tot;
