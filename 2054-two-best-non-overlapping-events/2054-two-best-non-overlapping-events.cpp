5595b06638f08c4703b3b585815a6ffb6975ac4d
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2025-12-26 20:40:02 +0530

    Time: 240 ms (6.59%), Space: 214.1 MB (5.03%) - LeetHub

--- 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
+++ 2054-two-best-non-overlapping-events/2054-two-best-non-overlapping-events.cpp
@@ -1,33 +1,41 @@
 class Solution {
 public:
-    bool isOverlapping(vector<int>a,vector<int>b){
-        return max(a[0],b[0])<=min(a[1],b[1]);
+    static bool compEnd(const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
     }
-    static bool comp(vector<int>&a,vector<int>&b){
-        return a[1]<b[1];
-        
+
+    static bool compStart(const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
     }
+
     int maxTwoEvents(vector<vector<int>>& events) {
+        int n = events.size();
 
-        sort(events.begin(),events.end(),comp);
-        unordered_map<int,int>pref;
-        vector<int>end;
-        int curr=0;
-        for(auto it:events){
-            curr=max(curr,it[2]);
-            pref[it[1]]=curr;
-            if(end.empty()||end.back()!=it[1]) end.push_back(it[1]);
+      
+        vector<vector<int>> endTime = events;
+        vector<vector<int>> startTime = events;
+
+        sort(endTime.begin(), endTime.end(), compEnd);
+        sort(startTime.begin(), startTime.end(), compStart);
+
+        int idx = 0;
+        int bestEndedValue = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+           
+            ans = max(ans, startTime[right][2]);
+
+            
+            while (idx < n && endTime[idx][1] < startTime[right][0]) {
+                bestEndedValue = max(bestEndedValue, endTime[idx][2]);
+                idx++;
+            }
+
+           
+            ans = max(ans, bestEndedValue + startTime[right][2]);
         }
-        int ans=curr;
 
-       for(auto it:events){
-        int start=it[0];
-        int idx=lower_bound(end.begin(),end.end(),start)-end.begin();
-        idx--;
-        if(idx>=0) ans=max(ans,(it[2]+pref[end[idx]]));
-       }
-
-       return ans;
-        
+        return ans;
     }
 };
