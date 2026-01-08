854d006c6b4eb6aa0ed9c7939d1a9f3684bdb485
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-01-08 22:22:50 +0530

    Time: 165 ms (5.2%), Space: 62.8 MB (5.6%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,35 +1,65 @@
 class Solution {
 public:
-    //max dot product between subarray nums1 from 0 to idx1 and subarray nums2 from 0 to idx2
-    int helper(int idx1,int idx2,vector<int>&nums1,vector<int>&nums2,bool flag,vector<vector<vector<int>>>&dp){
-        if(idx1<0||idx2<0) {
-            if(flag) return -1e9;
-            else return 0;
+    // max dot product between subarray nums1 from 0 to idx1 and subarray nums2
+    // from 0 to idx2
+    int helper(int idx1, int idx2, vector<int>& nums1, vector<int>& nums2,
+               bool flag, vector<vector<vector<int>>>& dp) {
+        if (idx1 < 0 || idx2 < 0) {
+            if (flag)
+                return -1e9;
+            else
+                return 0;
         }
-        if(dp[idx1][idx2][flag]!=-1) return dp[idx1][idx2][flag];
+        if (dp[idx1][idx2][flag] != -1)
+            return dp[idx1][idx2][flag];
 
-        //skip idx1
-        int op1=helper(idx1-1,idx2,nums1,nums2,flag,dp);
+        // skip idx1
+        int op1 = helper(idx1 - 1, idx2, nums1, nums2, flag, dp);
 
-        //skip idx2
-        int op2=helper(idx1,idx2-1,nums1,nums2,flag,dp);
+        // skip idx2
+        int op2 = helper(idx1, idx2 - 1, nums1, nums2, flag, dp);
 
-        //take product
+        // take product
 
-        int op3=(nums1[idx1]*nums2[idx2])+helper(idx1-1,idx2-1,nums1,nums2,false,dp);
+        int op3 = (nums1[idx1] * nums2[idx2]) +
+                  helper(idx1 - 1, idx2 - 1, nums1, nums2, false, dp);
 
-
-        return dp[idx1][idx2][flag]=max({op1,op2,op3});
-
-
+        return dp[idx1][idx2][flag] = max({op1, op2, op3});
     }
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        int n=nums1.size();
-        int m=nums2.size();
 
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(2,-1)));
+        // tc: o(2*n*m)  sc : o(n*m*2)+o(n+m)
+        int n = nums1.size();
+        int m = nums2.size();
 
-        return helper(n-1,m-1,nums1,nums2,true,dp);
-        
+        vector<vector<vector<int>>> dp(
+            n + 1, vector<vector<int>>(m + 1, vector<int>(2, -1e9)));
+
+        for (int i = 0; i <= n; i++)
+            dp[i][0][false] = 0;
+        for (int i = 0; i <= m; i++)
+            dp[0][i][false] = 0;
+
+        for (int idx1 = 1; idx1 <= n; idx1++) {
+            for (int idx2 = 1; idx2 <= m; idx2++) {
+                for (int flag = 0; flag <= 1; flag++) {
+
+                    // skip idx1
+                    int op1 = dp[idx1-1][idx2][flag];
+                    // skip idx2
+                    int op2 = dp[idx1][idx2-1][flag];
+
+                    // take product
+
+                    int op3 =
+                        (nums1[idx1-1] * nums2[idx2-1]) +
+                        dp[idx1-1][idx2-1][0];
+
+                    dp[idx1][idx2][flag] = max({op1, op2, op3});
+                }
+            }
+        }
+
+        return dp[n][m][1];
     }
 };
