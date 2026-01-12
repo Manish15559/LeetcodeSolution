d1801a401eb4d0c0e2c68dff0dca2196ccc175f6
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-01-12 09:32:50 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.8%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    void addMultiplier(stack<char>&st,string &ans){
+        string curr;
+        while(('a'<=st.top()&&st.top()<='z')){
+            curr.push_back(st.top());
+            st.pop();
+        }
+        reverse(curr.begin(),curr.end());
+        st.pop();
+        int mul=1;
+        int val=0;
+        while(!st.empty()&&'0'<=st.top()&&st.top()<='9'){
+            val=val+(mul*(st.top()-'0'));
+            mul=mul*10;
+            st.pop();
+        }
+        string s="";
+        while(val--){
+            s.append(curr);
+        }
+        if(st.empty()) ans.append(s);
+        else{
+            for(auto it:s){
+                st.push(it);
+            }
+        }
+        return;
+
+
+    }
+    void helper(int idx,stack<char>&st,string&ans,string &s){
+        if(s.size()==idx) return;
+        if('a'<=s[idx]&&s[idx]<='z'){
+            if(st.empty()) ans.push_back(s[idx]);
+            else st.push(s[idx]);
+        }
+        else if('0'<=s[idx]&&s[idx]<='9') st.push(s[idx]);
+        else if(s[idx]=='[') st.push(s[idx]);
+        else{
+            addMultiplier(st,ans);
+        }
+        helper((idx+1),st,ans,s);
+    }
+
+    string decodeString(string s) {
+        stack<char>st;
+        string ans="";
+        helper(0,st,ans,s);
+
+        return ans;
+        
+    }
+};
