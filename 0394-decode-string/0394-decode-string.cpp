9a73cba1e3d821fb2a23d60dc219a770d214e235
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-01-12 09:38:19 +0530

    Time: 0 ms (100%), Space: 9.6 MB (31.8%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -1,67 +1,36 @@
 class Solution {
 public:
-    void addMultiplier(stack<char>&st,string &ans){
-        string curr;
-        while(('a'<=st.top()&&st.top()<='z')){
-            curr.push_back(st.top());
-            st.pop();
-        }
-        reverse(curr.begin(),curr.end());
-        st.pop();
-        int mul=1;
-        int val=0;
-        while(!st.empty()&&'0'<=st.top()&&st.top()<='9'){
-            val=val+(mul*(st.top()-'0'));
-            mul=mul*10;
-            st.pop();
-        }
-        string s="";
-        while(val--){
-            s.append(curr);
-        }
-        if(st.empty()) ans.append(s);
-        else{
-            for(auto it:s){
-                st.push(it);
+    string decodeString(string s) {
+        stack<string> strSt;
+        stack<int> numSt;
+
+        string curr = "";
+        int num = 0;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0');   // handle multi-digit numbers
+            }
+            else if (c == '[') {
+                numSt.push(num);
+                strSt.push(curr);
+                num = 0;
+                curr = "";
+            }
+            else if (c == ']') {
+                int repeat = numSt.top(); numSt.pop();
+                string prev = strSt.top(); strSt.pop();
+
+                string temp = "";
+                while (repeat--) temp += curr;
+
+                curr = prev + temp;
+            }
+            else {
+                curr.push_back(c);
             }
         }
-        return;
 
-
-    }
-    void helper(int idx,stack<char>&st,string&ans,string &s){
-        if(s.size()==idx) return;
-        if('a'<=s[idx]&&s[idx]<='z'){
-            if(st.empty()) ans.push_back(s[idx]);
-            else st.push(s[idx]);
-        }
-        else if('0'<=s[idx]&&s[idx]<='9') st.push(s[idx]);
-        else if(s[idx]=='[') st.push(s[idx]);
-        else{
-            addMultiplier(st,ans);
-        }
-        helper((idx+1),st,ans,s);
-    }
-
-    string decodeString(string s) {
-        stack<char>st;
-        string ans="";
-
-        for(int idx=0;idx<s.size();idx++){
-            if('a'<=s[idx]&&s[idx]<='z'){
-            if(st.empty()) ans.push_back(s[idx]);
-            else st.push(s[idx]);
-        }
-        else if('0'<=s[idx]&&s[idx]<='9') st.push(s[idx]);
-        else if(s[idx]=='[') st.push(s[idx]);
-        else{
-            addMultiplier(st,ans);
-        }
-
-        }
-      
-
-        return ans;
-        
+        return curr;
     }
 };
