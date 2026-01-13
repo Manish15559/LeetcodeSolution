8b4ac64c819b75ca9085c36de645dcc8aa2d45c1
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-01-14 00:26:19 +0530

    Time: 95 ms (66.76%), Space: 198.8 MB (52.02%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.cpp
+++ 3453-separate-squares-i/3453-separate-squares-i.cpp
@@ -1,39 +1,42 @@
 class Solution {
 public:
-    // Compute the total area above y
-    static double area_geq_y(vector<vector<int>>& squares, const double y) {
-        double A=0;
-        for (auto& s : squares) {
-            double L=s[2], y0=s[1]+L; // y0 is the top boundary
-            if (y>=y0) continue;  // the square is below y
-
-            double h=min(L, y0-y);  // the valid height above y
-            A+=L*h;
+   
+     double find( double range,vector<vector<int>>&squares){
+         double ans=0;
+        for(auto &it:squares){
+            if(( double)it[1]>=range) continue;
+            else if(( double)(it[1]+it[2])<=range) ans+=(( double)it[2]*it[2]);
+            else{
+                 double length = (range-it[1]);
+                ans+=(length*it[2]);
+            }
         }
-        return A;
+        return ans;
+
     }
+    double separateSquares(vector<vector<int>>& squares) {
+      
+         double tot=0;
+          double low=1e9+10;
+         double high=-1;
+        for(auto &it:squares) {
+            tot+=((double)it[2]*it[2]);
+            low=min(low,(double)it[1]);
+            high=max(high,(double)(it[1]+it[2]));
+        }
+        
+        while(high-low>1e-5){
+            double mid=(low+high)/2;
+            double val= find(mid,squares);
+            if(tot>2*val) low=mid;
+            else high=mid;
+           
 
-    static double separateSquares(vector<vector<int>>& squares) {
-        double A0=area_geq_y(squares, -1e9);  // Compute total area
-        double A2=A0/2.0;  // Half the total area
-
-        double minY=INT_MAX, maxY=INT_MIN;
-        for (auto& s : squares) {
-            double L=s[2], y0=s[1];
-            minY=min(minY, y0);
-            maxY=max(maxY, y0+L);
         }
 
-        double l=minY, r=maxY, m;
-        const double epsilon=1e-5;  
+        return high;
 
-        while (r-l> epsilon) {
-            m=(l+r)/ 2;
-            double A=area_geq_y(squares, m);
 
-            if (A>A2) l=m;  
-            else r=m;  
-        }
-        return l;
+        
     }
 };
