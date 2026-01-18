b06527d228de0e6facc97e186b32ed09e8acaf9a
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-01-18 14:19:33 +0530

    Time: 7 ms (86.63%), Space: 15.1 MB (20.06%) - LeetHub

--- 1895-largest-magic-square/1895-largest-magic-square.cpp
+++ 1895-largest-magic-square/1895-largest-magic-square.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     int largestMagicSquare(vector<vector<int>>& grid) {
+
         int ans = 1;
         int n = grid.size();
         int m = grid[0].size();
+
+        vector<vector<int>>prefRow(n+1,vector<int>(m+1,0));
+        vector<vector<int>>prefCol(n+1,vector<int>(m+1,0));
+        vector<vector<int>>diagonal1(n+1,vector<int>(m+1,0));
+        vector<vector<int>>diagonal2(n+1,vector<int>(m+2,0));
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                prefRow[i][j]=prefRow[i][j-1]+grid[i-1][j-1];
+                prefCol[i][j]=prefCol[i-1][j]+grid[i-1][j-1];
+                diagonal1[i][j]=diagonal1[i-1][j-1]+grid[i-1][j-1];
+                diagonal2[i][j]=diagonal2[i-1][j+1]+grid[i-1][j-1];
+            }
+        }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 int len = min((i), (j));
@@ -14,11 +29,9 @@ public:
                     int y2 = j;
                     int val = -1;
                     bool f = true;
+
                     for (int row = x1; row <= x2; row++) {
-                        int curr = 0;
-                        for (int col = y1; col <= y2; col++) {
-                            curr += grid[row][col];
-                        }
+                        int curr = prefRow[row+1][y2+1]-prefRow[row+1][y1];
                         if (val == -1)
                             val = curr;
                         else if (val != curr) {
@@ -30,10 +43,8 @@ public:
                         continue;
 
                     for (int col = y1; col <= y2; col++) {
-                        int curr = 0;
-                        for (int row = x1; row <= x2; row++) {
-                            curr += grid[row][col];
-                        }
+                        int curr = prefCol[x2+1][col+1]-prefCol[x1][col+1];
+                    
 
                         if (val != curr) {
                             f = false;
@@ -44,18 +55,10 @@ public:
                         continue;
 
                     // upper diagonal
-                    int row = x1;
-                    int col = y1;
-                    int curr = 0;
-                    while (row <= x2 && col <= y2)
-                        curr += grid[row++][col++];
+                    int curr = diagonal1[x2+1][y2+1]-diagonal1[x1][y1];
                     if (curr != val)
                         continue;
-                    row = x2;
-                    col = y1;
-                    curr = 0;
-                    while (row >= x1 && col <= y2)
-                        curr += grid[row--][col++];
+                    curr =diagonal2[x2+1][y1+1]-diagonal2[x1][y2+2];
                     if (curr != val)
                         continue;
 
