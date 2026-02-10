30ad80bfad930f2ac5273f58bf08577e90f85a2d
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-02-10 09:05:53 +0530

    Time: 1879 ms (71.07%), Space: 530.5 MB (12.58%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+
+        int n=nums.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            unordered_set<int>st1,st2;
+            for(int j=i;j<n;j++){
+                if(nums[j]%2) st1.insert(nums[j]);
+                else st2.insert(nums[j]);
+
+                if(st1.size()==st2.size()) ans=max(ans,(j-i+1));
+
+            }
+        }
+        return ans;
+        
+    }
+};
