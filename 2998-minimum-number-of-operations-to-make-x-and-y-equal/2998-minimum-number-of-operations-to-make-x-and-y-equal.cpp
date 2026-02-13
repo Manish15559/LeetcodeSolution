923e009fc7a505f225ba30b625c639c6180d15e1
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-02-13 21:02:29 +0530

    Time: 1392 ms (5.02%), Space: 305.9 MB (5.02%) - LeetHub

---
+++ 2998-minimum-number-of-operations-to-make-x-and-y-equal/2998-minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int minimumOperationsToMakeEqual(int x, int y) {
+        if(x==y) return 0;
+        queue<pair<int,int>>q;
+        vector<int>vis(1e6,0);
+        q.push({x,0});
+        vis[x]=1;
+
+
+        while(!q.empty()){
+            int node=q.front().first;
+            int times=q.front().second;
+            q.pop();
+            // if(vis[node]==1) continue;
+            //divide by 11
+            if(node%11==0&&vis[node%11]==0){
+                if(node/11==y) return times+1;
+                q.push({(node/11),(times+1)});
+                vis[node/11]=1;
+            }
+            if(node%5==0&&vis[node%5]==0){
+                if(node/5==y) return times+1;
+                q.push({(node/5),(times+1)});
+                vis[node/5]=1;
+            }
+            if(node<1e6&&vis[(node+1)]==0){
+                if((node+1)==y) return times+1;
+                q.push({(node+1),(times+1)});
+                vis[(node+1)]=1;
+            }
+            if((node-1)>=0&&vis[(node-1)]==0){
+                if((node-1)==y) return times+1;
+                q.push({(node-1),(times+1)});
+                vis[(node-1)]=1;
+            }
+        }
+
+        return -1;
+        
+    }
+};
