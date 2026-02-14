738374f20e204f81c0b1dc6ef993018b0bc83422
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-02-14 16:16:07 +0530

    Time: 0 ms (100%), Space: 99.5 MB (78.55%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.cpp
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -4,6 +4,7 @@ public:
         int sum=0;
         int prev=0;
         int n=nums.size();
+        //Whenever you get struck on these kinds of questions, say for example, it will take O(N) time in solving for one permutation or anything similar to this, and it looks like you have to do this for all N permutations which will make it an overall O(N^2), then 9 out of 10 times there will be a way to calculate the ans for the next permutation if you have previous in constant O(1) time (this is the same case here), or maybe it might take O(logN) time. Always remeber that when you can't come up with better solution then O(N^2).
         for(int i=0;i<n;i++){
             sum+=nums[i];
             prev+=(i*nums[i]);
