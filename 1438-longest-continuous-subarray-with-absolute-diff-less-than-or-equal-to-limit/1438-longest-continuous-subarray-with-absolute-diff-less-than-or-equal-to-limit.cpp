df58b57cf21faadc38680b84e1cfb77152713984
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-02-23 22:11:33 +0530

    Time: 27 ms (55.92%), Space: 77.5 MB (77.72%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        int n=nums.size();
-        multiset<int>st;
         int left=0;
+        deque<int>maxi,mini;
         int ans=1;
-        for(int right=0;right<n;right++){
-            st.insert(nums[right]);
+        for(int right=0;right<nums.size();right++){
+           while(!maxi.empty()&&maxi.back()<nums[right]) maxi.pop_back();
+           while(!mini.empty()&&mini.back()>nums[right]) mini.pop_back();
+           maxi.push_back(nums[right]);
+           mini.push_back(nums[right]);
 
-            while(abs(*st.rbegin()-*st.begin())>limit) {
-                st.erase(st.find(nums[left]));
-                left++;
-            }
-            ans=max(ans,(right-left+1));
+           while((maxi.front()-mini.front())>limit){
+            if(maxi.front()==nums[left]) maxi.pop_front();
+            if(mini.front()==nums[left]) mini.pop_front();
+            left++;
+           }
+           ans=max(ans,(right-left+1));
+
+
+          
 
         }
         return ans;
