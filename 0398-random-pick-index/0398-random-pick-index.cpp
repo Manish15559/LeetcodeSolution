17c3d6e7954c8424df1900a9c37d7623d9393b94
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-02-26 20:14:10 +0530

    Time: 38 ms (84.6%), Space: 66.7 MB (60.34%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> cnt;
+    Solution(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            cnt[nums[i]].push_back(i);
+        }
+    }
+
+    int pick(int target) {
+        int sz = cnt[target].size();
+        int idx = rand() % sz;
+        return cnt[target][idx];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
