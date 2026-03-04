bf654a6025629073af94bb73abebe786e2724711
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-03-04 17:18:55 +0530

    Time: 0 ms (100%), Space: 29.4 MB (91.47%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    void reverseList(vector<int>&arr,int left,int right){
+        while(left<right){
+            swap(arr[left],arr[right]);
+            left++;
+            right--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
 
         // 1 2 3 4 5 6 7  => k=3 => 5 6 7 1 2 3 4
@@ -9,22 +16,33 @@ public:
         // 5 6 7 => temp
         // 1 2 3 4 5 6 7 =>
 
-        vector<int>temp;
+        // vector<int>temp;
+        // int n=nums.size();
+        // k%=n;
+        // for(int i=n-k;i<n;i++){
+        //     temp.push_back(nums[i]);
+
+        // }
+
+        // for(int i=n-k-1;i>=0;i--){
+        //     swap(nums[i],nums[i+k]);
+        // }
+
+        // for(int i=0;i<k;i++){
+        //     nums[i]=temp[i];
+        // }
+
+        // 1 2 3 4 5 => k=2 => 4 5 1 2 3
+        // first (n-k) element + last k elemnt  => last k element + first (n-k) element
+        //                                             5 4 3 2 1
+        //                                             4 5 3 2 1
+        //                                             4 5 1 2 3
+
         int n=nums.size();
         k%=n;
-        for(int i=n-k;i<n;i++){
-            temp.push_back(nums[i]);
-
-        }
-
-        for(int i=n-k-1;i>=0;i--){
-            swap(nums[i],nums[i+k]);
-        }
-
-        for(int i=0;i<k;i++){
-            nums[i]=temp[i];
-        }
-
+        reverseList(nums,0,n-1);
+        reverseList(nums,0,k-1);
+        reverseList(nums,k,n-1);
       
     }
 };
