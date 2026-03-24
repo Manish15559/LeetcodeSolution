fbd64922fd000f4bfc99ac313bdbe26c450097c5
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-03-25 00:38:04 +0530

    Time: 371 ms (7.19%), Space: 83.6 MB (26.22%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
-    long long way(int idx,int pos,string &target,vector<vector<int>>&count,vector<vector<int>>&dp){
-        if(idx==target.size()) return 1;
-        if(pos==count[0].size()) return 0;
-        if(dp[idx][pos]!=-1) return dp[idx][pos];
-        long long ways=0;
-        int mod=1e9+7;
-        for(char ch='a';ch<='z';ch++){
-            if(target[idx]==ch&&count[(ch-'a')][pos]) ways+=(count[(ch-'a')][pos]*way(idx+1,pos+1,target,count,dp));
-            ways%=mod;
+    long long way(int idx, int pos, string& target, vector<vector<int>>& count,
+                  vector<vector<int>>& dp) {
+        if (idx == target.size())
+            return 1;
+        if (pos == count[0].size())
+            return 0;
+        if (dp[idx][pos] != -1)
+            return dp[idx][pos];
+        long long ways = 0;
+        int mod = 1e9 + 7;
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (target[idx] == ch && count[(ch - 'a')][pos])
+                ways += (count[(ch - 'a')][pos] *
+                         way(idx + 1, pos + 1, target, count, dp));
+            ways %= mod;
         }
-        ways+=way(idx,pos+1,target,count,dp);
-        ways%=mod;
-        return dp[idx][pos]=ways;
+        ways += way(idx, pos + 1, target, count, dp);
+        ways %= mod;
+        return dp[idx][pos] = ways;
     }
     int numWays(vector<string>& words, string target) {
 
-        int n=words[0].size();
-        vector<vector<int>>count(26,vector<int>(n,0));
-        int m=target.size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            for(auto &word:words) count[(word[i]-'a')][i]++;
+        int n = words[0].size();
+        vector<vector<int>> count(26, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            for (auto& word : words)
+                count[(word[i] - 'a')][i]++;
+        }
+        int m = target.size();
+        vector<vector<long long>> dp(m + 1, vector<long long>(n + 1, 0));
+        for (int i = 0; i <= n; i++)
+            dp[m][i] = 1;
+        for (int idx = m - 1; idx >= 0; idx--) {
+            for (int pos = n - 1; pos >= 0; pos--) {
+                long long ways = 0;
+                int mod = 1e9 + 7;
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    if (target[idx] == ch && count[(ch - 'a')][pos])
+                        ways += (count[(ch - 'a')][pos] * dp[idx + 1][pos + 1]);
+                    ways %= mod;
+                }
+                ways += dp[idx][pos + 1];
+                ways %= mod;
+                dp[idx][pos] = ways;
+            }
         }
 
-        return way(0,0,target,count,dp);
-        
+        return dp[0][0];
     }
 };
