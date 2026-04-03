55b5eca0cfa0d226474d6f840fea3c1099b123bd
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-04-03 23:25:08 +0530

    Time: 0 ms (100%), Space: 20.5 MB (60.77%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, (n - 1)); // minimum jump fron i to reach n-1 index;
-        dp[n - 1] = 0;
-        for (int i = n - 2; i >= 0; i--) {
+       int n=nums.size();
+       int jumps=0;
+       int rangeEnd=0;
+       int farthest=0;
+       for(int i=0;i<n-1;i++){
+        farthest=max(farthest,(i+nums[i]));
 
-            for (int j = i + 1; j <= min((n - 1), (i + nums[i])); j++) {
-                dp[i] = min(dp[i], (dp[j] + 1));
-            }
+        if(i==rangeEnd){
+            jumps++;
+            rangeEnd=farthest;
         }
+       }
 
-        return dp[0];
+       return jumps;
     }
 };
