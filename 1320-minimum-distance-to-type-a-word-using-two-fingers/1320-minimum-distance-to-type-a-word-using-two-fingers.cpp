920a411a1d1070bdeffa627a3dc3cc0eaf0515b5
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-04-12 13:55:16 +0530

    Time: 55 ms (28.92%), Space: 46.5 MB (25.3%) - LeetHub

---
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int helper(int idx, int ch1, int ch2, string& word,
+               vector<vector<vector<int>>>& dp) {
+                // cout<<ch1<<" "<<ch2<<endl;
+
+        if (idx == word.size())
+            return 0;
+        if (dp[idx][ch1][ch2] != -1)
+            return dp[idx][ch1][ch2];
+        int row1 = ch1 / 6;
+        int col1 = ch1 % 6;
+        int row2 = ch2 / 6;
+        int col2 = ch2 % 6;
+        int row = (word[idx] - 'A') / 6;
+        int col = (word[idx] - 'A') % 6;
+        int op1 = abs(row1 - row) + abs(col1 - col) +
+                  helper(idx + 1, (word[idx] - 'A'), ch2, word, dp);
+        int op2 = abs(row2 - row) + abs(col2 - col) +
+                  helper(idx + 1, ch1, (word[idx] - 'A'), word, dp);
+
+        return dp[idx][ch1][ch2] = min(op1, op2);
+    }
+    int minimumDistance(string word) {
+
+        int n = word.size();
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(26, vector<int>(26, -1)));
+
+        int mini = 1e9;
+        for (int ch1 = 0; ch1 < 26; ch1++) {
+            for (int ch2 = 0; ch2 < 26; ch2++) {
+                mini = min(mini, helper(0, ch1, ch2, word, dp));
+            }
+        }
+
+        return mini;
+    }
+};
