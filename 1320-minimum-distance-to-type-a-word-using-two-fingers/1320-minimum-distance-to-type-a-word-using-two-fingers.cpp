7d2afb071c8f7c8fba6447aca04661f71eaf6842
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-04-12 13:58:55 +0530

    Time: 87 ms (17.27%), Space: 46.7 MB (24.9%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int helper(int idx, int ch1, int ch2, string& word,
                vector<vector<vector<int>>>& dp) {
-                // cout<<ch1<<" "<<ch2<<endl;
+        // cout<<ch1<<" "<<ch2<<endl;
 
         if (idx == word.size())
             return 0;
@@ -25,12 +25,35 @@ public:
 
         int n = word.size();
         vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(26, vector<int>(26, -1)));
+            n + 1, vector<vector<int>>(26, vector<int>(26, 1e9)));
 
         int mini = 1e9;
         for (int ch1 = 0; ch1 < 26; ch1++) {
             for (int ch2 = 0; ch2 < 26; ch2++) {
-                mini = min(mini, helper(0, ch1, ch2, word, dp));
+                dp[n][ch1][ch2] = 0;
+            }
+        }
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int ch1 = 0; ch1 < 26; ch1++) {
+                for (int ch2 = 0; ch2 < 26; ch2++) {
+                    int row1 = ch1 / 6;
+                    int col1 = ch1 % 6;
+                    int row2 = ch2 / 6;
+                    int col2 = ch2 % 6;
+                    int row = (word[idx] - 'A') / 6;
+                    int col = (word[idx] - 'A') % 6;
+                    int op1 = abs(row1 - row) + abs(col1 - col) +
+                              dp[idx + 1][(word[idx] - 'A')][ch2];
+                    int op2 = abs(row2 - row) + abs(col2 - col) +
+                              dp[idx + 1][ch1][(word[idx] - 'A')];
+
+                    dp[idx][ch1][ch2] = min(op1, op2);
+                }
+            }
+        }
+        for (int ch1 = 0; ch1 < 26; ch1++) {
+            for (int ch2 = 0; ch2 < 26; ch2++) {
+                mini = min(mini, dp[0][ch1][ch2]);
             }
         }
 
