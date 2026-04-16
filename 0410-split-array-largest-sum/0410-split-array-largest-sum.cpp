e39f61e73d1db4c65a492b08ff1018ed02afe291
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-04-16 20:43:07 +0530

    Time: 0 ms (100%), Space: 10.6 MB (38.38%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,39 +1,53 @@
 class Solution {
 public:
-   bool check(long long mid,int k,vector<int>&nums){
-    int cnt=1;
-    long long sum=0;
-    for(auto it:nums){
-        if((sum+it)>mid){
-            cnt++;
-            sum=it;
-        }
-        else{
-            sum+=it;
+    bool check1(long long mid, int k, vector<int>& nums) {
+        int cnt = 1;
+        long long sum = 0;
+        for (auto it : nums) {
+            if ((sum + it) > mid) {
+                cnt++;
+                sum = it;
+            } else {
+                sum += it;
+            }
         }
 
+        return cnt <= k;
     }
-    // cout<<cnt<<endl;
 
-    return cnt<=k;
-   }
+    bool check2(long long mid, int k, vector<int>& nums) {
+        long long remaining = mid;
+        int cnt = 1;
+        for (auto it : nums) {
+            if ((remaining - it) < 0) {
+                cnt++;
+                remaining = mid - it;
 
+            } else {
+                remaining -= it;
+            }
+        }
+        return cnt <= k;
+    }
 
     int splitArray(vector<int>& nums, int k) {
 
-        long long low=*max_element(nums.begin(),nums.end());
-        long long high=0;
-        for(auto it:nums) high+=it;
+        long long low = *max_element(nums.begin(), nums.end());
+        long long high = 0;
+        for (auto it : nums)
+            high += it;
 
-        while(high-low>1){
-            long long mid=(low+high)/2;
-            cout<<mid<<endl;
-            if(check(mid,k,nums)) high=mid;
-            else low=mid+1;
+        while (high - low > 1) {
+            long long mid = (low + high) / 2;
+            // cout<<mid<<endl;
+            if (check2(mid, k, nums))
+                high = mid;
+            else
+                low = mid + 1;
         }
 
-        if(check(low,k,nums)) return low;
+        if (check2(low, k, nums))
+            return low;
         return high;
-        
     }
 };
