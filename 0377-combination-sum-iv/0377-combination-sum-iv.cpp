b77ba78005e530c58df6c19daad13660b4e1581f
Manish Sah <114980892+Manish15559@users.noreply.github.com>
2026-04-22 23:51:00 +0530

    Time: 0 ms (100%), Space: 9.3 MB (19.62%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    int helper(int target, vector<int>& nums,vector<int>& dp) {
-       if(target==0) return 1;
-       if(dp[target]!=-1) return dp[target];
+    int helper(int target, vector<int>& nums, vector<int>& dp) {
+        if (target == 0)
+            return 1;
+        if (dp[target] != -1)
+            return dp[target];
 
-       int op=0;
-       for(auto it:nums){
-        if(it<=target) op+=helper(target-it,nums,dp);
-       }
-       return dp[target]=op;
+        int op = 0;
+        for (auto it : nums) {
+            if (it <= target)
+                op += helper(target - it, nums, dp);
+        }
+        return dp[target] = op;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,-1);
-        return helper(target, nums, dp);
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            long long op = 0;
+            for (auto it : nums) {
+                if (it <= i)
+                    op += dp[(i - it)];
+            }
+            dp[i] = op;
+        }
+        return dp[target];
     }
 };
